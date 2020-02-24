b45ab6763b3054654252ef0642a759c0bd7dc99d
Lucas Gabriel <lucas.gdsouza@outlook.com>
2020-02-23 22:19:31 -0300

    View Init

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -6,7 +6,10 @@ class NegociacaoController{
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-		this._listaNegociacoes = new ListaNegociacoes();
+        this._listaNegociacoes = new ListaNegociacoes();
+        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
+
+        this._negociacoesView.update();
     }
 
     /*Programação Defensiva protegendo a Lista de Negociações.
@@ -18,8 +21,9 @@ class NegociacaoController{
 	
         this._listaNegociacoes.adiciona(this._criaNegociacao());
         //this._listaNegociacoes.negociacoes.push(this._criaNegociacao());
+        this._negociacoesView.update(this._listaNegociacoes);
         this._limpaFormulario();		
-		//console.log(this._listaNegociacoes._negociacoes);
+        //console.log(this._listaNegociacoes._negociacoes);
     }
 
     _criaNegociacao(){
@@ -32,7 +36,7 @@ class NegociacaoController{
     _limpaFormulario(){
         this._inputData.value = "";
         this._inputQuantidade.value = 1;
-        this._inputData.value = 1;
+        this._inputValor.value = 1;
 
         this._inputData.focus();
     }
