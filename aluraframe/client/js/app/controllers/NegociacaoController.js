5f684b3d2cdaa44e66ff56ef2dcff041a5d20c55
Lucas Gabriel <lucas.gdsouza@outlook.com>
2020-03-08 22:07:38 -0300

    Observer Pattern

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -7,7 +7,7 @@ class NegociacaoController{
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        this._listaNegociacoes = new ListaNegociacoes();
+        this._listaNegociacoes = new ListaNegociacoes(model => this._negociacoesView.update(model));
 
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
@@ -26,9 +26,7 @@ class NegociacaoController{
         this._listaNegociacoes.adiciona(this._criaNegociacao());
         this._negociacoesView.update(this._listaNegociacoes);
     
-        this._mensagem.texto = 'Negociacao adicionada com sucesso';
-        this._mensagemView.update(this._mensagem);  
-    
+        this._mensagem.texto = 'Negociacao adicionada com sucesso';    
         this._limpaFormulario();
     }
     
@@ -38,6 +36,12 @@ class NegociacaoController{
             DataHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value);    
+    }
+
+    apaga(){
+        this._listaNegociacoes.esvazia();
+        this._mensagem.texto = 'Negociações apagadas com sucesso!';
+        this._mensagemView.update(this._mensagem);
     }
     
     _limpaFormulario() {
